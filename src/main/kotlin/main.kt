ccb9a1b3aeab0b9d72d23ed34ef07f25a40ce928
Rus Galeev <spyder44@yandex.ru>
2022-04-02 10:17:05 +0300

    edit main.kt fun countLikes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,16 @@
-var likes = 1234
+var likes = 310
 fun main(){
     println(countLikes(likes))
 }
 
 fun countLikes(likes: Int):String{
     if (likes <= 0 ) {return ""}
-    return "Понравилось $likes ${when(likes % 1000 % 100 / 10) {
-        0 -> "человеку"
-        in 1..9 -> "людям"
-        else -> ""}}"
+    return "Понравилось $likes ${
+        when (likes % 100) {
+            10, 11 -> "людям"
+            else -> when(likes % 10) {
+                0, 1 -> "человеку"
+                in 2..9 -> "людям"
+                else -> ""}}
+        }"
 }
